2123ebe5a11c536190ff4c0bd7558d6c9766031b
xieyi <yxie@nbai.io>
2021-11-09 11:24:00 +0800

    add files

--- config/dev.env.js
+++ config/dev.env.js
@@ -4,6 +4,5 @@ const prodEnv = require('./prod.env')
 
 module.exports = merge(prodEnv, {
   NODE_ENV: '"development"',
-  BASE_API: '"https://staging.filswan.com/"',
-  BASE_PAYMENT_GATEWAY_API: '"https://staging.filswan.com/"'
+  BASE_API: '"https://staging.filswan.com/"'
 })
