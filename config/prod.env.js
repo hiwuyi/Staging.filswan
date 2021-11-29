6cebd2e86e2764d07a16a8e9b169c684a9c261d4
xieyi <yxie@nbai.io>
2021-11-29 16:21:36 +0800

    logo replace

--- config/prod.env.js
+++ config/prod.env.js
@@ -1,7 +1,7 @@
 'use strict'
 module.exports = {
   NODE_ENV: '"production"',
-  // BASE_API: '"https://staging.filswan.com/"',
+  // BASE_API: '"https://mcp.filswan.com/"',
   BASE_API: '"http://192.168.88.216:5002/"',
   BASE_PAYMENT_GATEWAY_API: '"http://192.168.88.217:8889/"'
 }
