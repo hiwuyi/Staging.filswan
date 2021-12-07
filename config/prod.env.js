cc5914e5c3ce6c071996741f999fe4d4a28b92a9
xieyi <yxie@nbai.io>
2021-12-07 10:20:37 +0800

    prod config

--- config/prod.env.js
+++ config/prod.env.js
@@ -4,4 +4,6 @@ module.exports = {
   // BASE_API: '"https://mcp.filswan.com/"',
   BASE_API: '"http://192.168.88.216:5002/"',
   BASE_PAYMENT_GATEWAY_API: '"http://192.168.88.217:8889/"'
+  // BASE_API: '"https://calibration-api.filswan.com/"',
+  // BASE_PAYMENT_GATEWAY_API: '"https://calibration-mcp-api.filswan.com/"'
 }
